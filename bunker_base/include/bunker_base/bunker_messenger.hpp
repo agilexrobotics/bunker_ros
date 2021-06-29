4af3c217c93084db1fc1352103f3a8d517461a78
agilexrobotics <53333772+agilexrobotics@users.noreply.github.com>
2021-06-29 13:41:45 +0800

    Add files via upload
    first commit

---
+++ bunker_base/include/bunker_base/bunker_messenger.hpp
@@ -0,0 +1,74 @@
+/*
+ * bunker_messenger.hpp
+ * 
+ * Created on: Jun 14, 2019 10:24
+ * Description: 
+ * 
+ * Copyright (c) 2019 Ruixiang Du (rdu)
+ */
+
+#ifndef BUNKER_MESSENGER_HPP
+#define BUNKER_MESSENGER_HPP
+
+#include <string>
+
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+// #include <tf/transform_broadcaster.h>
+#include <tf2_ros/transform_broadcaster.h>
+
+#include "bunker_msgs/BunkerLightCmd.h"
+#include "ugv_sdk/bunker/bunker_base.hpp"
+
+namespace westonrobot
+{
+class BunkerROSMessenger
+{
+public:
+    explicit BunkerROSMessenger(ros::NodeHandle *nh);
+    BunkerROSMessenger(BunkerBase *bunker, ros::NodeHandle *nh);
+
+    std::string odom_frame_;
+    std::string base_frame_;
+    std::string odom_topic_name_;
+
+    bool simulated_robot_ = false;
+    int sim_control_rate_ = 50;
+
+    void SetupSubscription();
+
+    void PublishStateToROS();
+    void PublishSimStateToROS(double linear, double angular);
+
+    void GetCurrentMotionCmdForSim(double &linear, double &angular);
+
+private:
+    BunkerBase *bunker_;
+    ros::NodeHandle *nh_;
+
+    std::mutex twist_mutex_;
+    geometry_msgs::Twist current_twist_;
+
+    ros::Publisher odom_publisher_;
+    ros::Publisher status_publisher_;
+    ros::Subscriber motion_cmd_subscriber_;
+    ros::Subscriber light_cmd_subscriber_;
+    tf2_ros::TransformBroadcaster tf_broadcaster_;
+
+    // speed variables
+    double linear_speed_ = 0.0;
+    double angular_speed_ = 0.0;
+    double position_x_ = 0.0;
+    double position_y_ = 0.0;
+    double theta_ = 0.0;
+
+    ros::Time last_time_;
+    ros::Time current_time_;
+
+    void TwistCmdCallback(const geometry_msgs::Twist::ConstPtr &msg);
+    void LightCmdCallback(const bunker_msgs::BunkerLightCmd::ConstPtr &msg);
+    void PublishOdometryToROS(double linear, double angular, double dt);
+};
+} // namespace westonrobot
+
+#endif /* BUNKER_MESSENGER_HPP */
